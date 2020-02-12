8d5b4af7d6c5b3c05dc7a6f82e8a6927e99b1c03
Paul Spicer <pdspicer@gmail.com>
2020-02-12 00:48:34 -0500

    feat: default manager export from main

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
-export { StorageManager } from './StorageManager';
+import {StorageManager} from './StorageManager';
+
+export {StorageManager};
 export * from './Storage';
 export * from './types';
+export const manager = new StorageManager({disks: {}});
